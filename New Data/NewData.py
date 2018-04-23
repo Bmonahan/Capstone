2f51103435b30da295eb989d488702edd674c214
Brysen Monahan <monahan.brysen@gmail.com>
2018-04-23 18:51:21 -0400

    changed spsv to percent

--- New Data/NewData.py
+++ New Data/NewData.py
@@ -20,11 +20,11 @@ with open('NHL_2011_2018.csv','w') as new_file, open('sort_data.txt','w') as sDa
             satTm = str(sat['data'][x]['teamAbbrev'])
             if satGID == gameId and satTm == teamAbb:
                 index = x
-                print x
+                #print x
                 SATP = str(sat['data'][x]['shotAttemptsPctg'])
                 OZF = str(sat['data'][x]['offensiveZoneFaceoffs'])
                 DZF = str(sat['data'][x]['defensiveZoneFaceoffs'])
-                SPSv = str(sat['data'][x]['shootingPlusSavePctg'])
+                SPSv = str(sat['data'][x]['shootingPlusSavePctg']*100)
 
         # sDat.write(teamAbb+"\n")
         # sDat.write(gameId+"\n")
