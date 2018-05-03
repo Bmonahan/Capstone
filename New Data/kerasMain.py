7cb6b92c7340366361e5ddd743a64355368db907
Brysen Monahan <monahan.brysen@gmail.com>
2018-05-02 21:02:46 -0400

    Added 83 percent acc model.Changed model loader to use it.

--- New Data/kerasMain.py
+++ New Data/kerasMain.py
@@ -26,21 +26,23 @@ games = pd.read_csv('11_18_raw.csv',sep=',')
 
 #print games
 
-X = games.ix[:,0:18] #All stats
-y = games.ix[:,18] #Wins or losses
+X = games.ix[:,0:16] #All stats
+y = games.ix[:,16] #Wins or losses
 
 # ynum = y.values
 
 
-X_train, X_test,y_train,y_test = train_test_split(X, y, test_size=0.33, random_state=seed, shuffle=True)
+X_train, X_test,y_train,y_test = train_test_split(X, y, test_size=0.27, random_state=seed, shuffle=True)
 # X_train = scaler.transform(X_train)
 # X_test = scaler.transform(X_test)
 def quadDense():
     # create model
     model = Sequential()
-    model.add(Dense(18,input_shape=(18,), kernel_initializer='normal'))
+    model.add(Dense(16,input_shape=(16,), kernel_initializer='normal'))
     model.add(Activation('relu'))
-    model.add(Dense(9, kernel_initializer='normal'))
+    model.add(Dense(8, kernel_initializer='normal'))
+    model.add(Activation('relu'))
+    model.add(Dense(4, kernel_initializer='normal'))
     model.add(Activation('relu'))
     model.add(Dense(1, kernel_initializer='normal'))
     model.add(Activation('sigmoid'))
@@ -58,7 +60,7 @@ weight = model.get_weights()
 
 logger = CSVLogger('log.csv',separator=',',append=False)
 start_time = time.time()
-fit = model.fit(X_train, y_train, validation_split=0.33,epochs=20, batch_size=10, verbose=1,callbacks=[logger])
+fit = model.fit(X_train, y_train, validation_split=0.33,epochs=25, batch_size=4, verbose=1,callbacks=[logger])
 
 
 print '----------------'
@@ -74,28 +76,14 @@ predictions = model.predict(X)
 rounded = [round(x[0]) for x in predictions]
 #print(rounded)
 print len(rounded)
-outcome = pd.read_csv('NHL_2011_2018.csv',sep=',')
-
-o = games.ix[:,4] #Wins or losses
-counted = []
-for x in range(0,len(rounded)):
-    if(o[x]==rounded[x]):
-        counted.append("correct")
-
-print len(counted)
 
 print '--------------------------------------------------------'
-countF = open('count.txt','r')
-count = int(countF.read())
-countStr = str(count)
-print type(count)
-countF.close()
 
 model_json = model.to_json()
-with open("model"+'_'+countStr+".json", "w") as json_file:
+with open("model_83pctg.json", "w") as json_file:
     json_file.write(model_json)
 # serialize weights to HDF5
-model.save_weights("model.h5")
+model.save_weights("model_83pctg.h5")
 print("Saved model to disk")
 
 # json_file = open('model.json', 'r')
